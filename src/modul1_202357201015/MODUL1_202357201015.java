65f4ea64d40848e7c56dfc39bfba58728036db4e
nurul <nurulkhoiyah86@gmail.com>
2024-03-20 21:54:42 +0700

    data[baris][kolom] + " "

--- src/modul1_202357201015/MODUL1_202357201015.java
+++ src/modul1_202357201015/MODUL1_202357201015.java
@@ -15,6 +15,11 @@ public class MODUL1_202357201015 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int data[][] = {{4,6,4,2,8,4,2,10},{4,6,4,2,8,4,2,10}};
+        for(int baris= 0; baris <data.length; baris++){
+            for(int kolom= 0; kolom <data[baris].length; kolom++){
+            System.out.print(data[baris][kolom] + " ");
+        }
     }
     
 }
